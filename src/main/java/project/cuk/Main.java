7f4df54de73a35f40997dd4cbe9d3780cee77636
Haadi Tasleem Zargar <haadijan1234561@gmail.com>
2026-03-21 16:16:40 +0530

    made the buttons ui more flexible

--- src/main/java/project/cuk/Main.java
+++ src/main/java/project/cuk/Main.java
@@ -21,7 +21,7 @@ public class Main extends Application{
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/Main.fxml"));
-        Scene scene = new Scene(root, 720, 480);
+        Scene scene = new Scene(root, 1080, 720);
         stage.setTitle("MiniWebBrowser");
 
         stage.setScene(scene);
